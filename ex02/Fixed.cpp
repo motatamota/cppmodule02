0928d1dd3895260cab3721a25a2d0135673d8930
tashiget <tashiget@student.42tokyo.jp>
2024-12-10 21:39:34 +0900

    ex02

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,172 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashiget <tashiget@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 18:36:15 by tashiget          #+#    #+#             */
+/*   Updated: 2024/12/10 18:36:15 by tashiget         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed() : _fixedPointValue(0)
+{
+}
+
+Fixed::Fixed(int num)
+{
+	setRawBits(num<<8);
+}
+
+Fixed::Fixed(float num)
+{
+	setRawBits(roundf(num * 256));
+}
+
+Fixed::Fixed(const Fixed& fixed)
+{
+	*this = fixed;
+}
+
+Fixed&	Fixed::operator=(const Fixed& other)
+{
+	_fixedPointValue = other.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+}
+
+int	Fixed::getRawBits(void) const
+{
+	return (_fixedPointValue);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	_fixedPointValue = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	return (static_cast<float>(_fixedPointValue) / static_cast<float>(256));
+}
+
+int		Fixed::toInt(void) const
+{
+	return (_fixedPointValue>>8);
+}
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& fixed)
+{
+	stream << fixed.toFloat();
+	return stream;
+}
+
+bool	Fixed::operator>(const Fixed& left) const
+{
+	return (this->getRawBits() > left.getRawBits());
+}
+
+bool	Fixed::operator<(const Fixed& left) const
+{
+	return (this->getRawBits() < left.getRawBits());
+}
+
+bool	Fixed::operator>=(const Fixed& left) const
+{
+	return (this->getRawBits() >= left.getRawBits());
+}
+
+bool	Fixed::operator<=(const Fixed& left) const
+{
+	return (this->getRawBits() <= left.getRawBits());
+}
+
+bool	Fixed::operator==(const Fixed& left) const
+{
+	return (this->getRawBits() == left.getRawBits());
+}
+
+bool	Fixed::operator!=(const Fixed& left) const
+{
+	return (this->getRawBits() != left.getRawBits());
+}
+
+Fixed	Fixed::operator+(const Fixed& left)
+{
+	Fixed tmp;
+	tmp.setRawBits(this->getRawBits() + left.getRawBits());
+	return (tmp);
+}
+
+Fixed	Fixed::operator-(const Fixed& left)
+{
+	Fixed tmp;
+	tmp.setRawBits(this->getRawBits() - left.getRawBits());
+	return (tmp);
+}
+
+Fixed	Fixed::operator*(const Fixed& left)
+{
+	Fixed tmp;
+	tmp.setRawBits(roundf(this->toFloat() * left.toFloat() * 256));
+	return (tmp);
+}
+
+Fixed	Fixed::operator/(const Fixed& left)
+{
+	Fixed tmp;
+	tmp.setRawBits(roundf(this->toFloat() / left.toFloat() * 256));
+	return (tmp);
+}
+
+Fixed& Fixed::min(Fixed& left, Fixed& right) {
+	return ((left <= right) ? left : right);
+}
+
+const Fixed& Fixed::min(const Fixed& left, const Fixed& right) {
+	return ((left <= right) ? left : right);
+}
+
+Fixed& Fixed::max(Fixed& left, Fixed& right) {
+	return ((left >= right) ? left : right);
+}
+
+const Fixed& Fixed::max(const Fixed& left, const Fixed& right) {
+	return ((left >= right) ? left : right);
+}
+
+Fixed&	Fixed::operator++()
+{
+	this->setRawBits(this->getRawBits() + 1);
+	return (*this);
+}
+
+Fixed	Fixed::operator++(int)
+{
+	Fixed tmp(*this);
+
+	this->setRawBits(this->getRawBits() + 1);
+	return (tmp);
+}
+
+Fixed&	Fixed::operator--()
+{
+	this->setRawBits(this->getRawBits() - 1);
+	return (*this);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed tmp(*this);
+
+	this->setRawBits(this->getRawBits() - 1);
+	return (tmp);
+}
---
+++ ex02/Fixed.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tashiget <tashiget@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 18:36:22 by tashiget          #+#    #+#             */
+/*   Updated: 2024/12/10 18:36:22 by tashiget         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+# include <iostream>
+# include <cmath>
+# include <bitset>
+
+class	Fixed
+{
+private:
+	int _fixedPointValue;
+	static const int _fractionalBits;
+public:
+	Fixed();
+	Fixed(int num);
+	Fixed(float num);
+	Fixed(const Fixed& fixed);
+	Fixed&	operator=(const Fixed& other);
+	~Fixed();
+	int					getRawBits(void) const;
+	void				setRawBits(int const raw);
+	float				toFloat(void) const;
+	int					toInt(void) const;
+	bool				operator>(const Fixed& left) const;
+	bool				operator<(const Fixed& left) const;
+	bool				operator>=(const Fixed& left) const;
+	bool				operator<=(const Fixed& left) const;
+	bool				operator==(const Fixed& left) const;
+	bool				operator!=(const Fixed& left) const;
+	Fixed				operator+(const Fixed& left);
+	Fixed				operator-(const Fixed& left);
+	Fixed				operator*(const Fixed& left);
+	Fixed				operator/(const Fixed& left);
+	Fixed&				operator++();
+	Fixed				operator++(int);
+	Fixed&				operator--();
+	Fixed				operator--(int);
+	static Fixed&		min(Fixed& left, Fixed& right);
+	static const Fixed&	min(const Fixed& left, const Fixed& right);
+	static Fixed&		max(Fixed& left, Fixed& right);
+	static const Fixed&	max(const Fixed& left, const Fixed& right);
+};
+
+std::ostream&	operator<<(std::ostream& stream, const Fixed& fixed);
+
+#endif
